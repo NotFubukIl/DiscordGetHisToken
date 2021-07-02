c84614d381cd6d000f60edbec8a94b8ae364962a
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-07-02 17:49:42 +0200

    Update gettoken.js

--- gettoken.js
+++ gettoken.js
@@ -1,12 +1,9 @@
-const question = require("readline-sync");
-
+const question = require("readline-sync")
 const request = require("sync-request")
-
 require('colors')
 
 const email = question.question(`Veuillez Entrer Votre Email: `.yellow)
 const password = question.question(`Veuillez Entrer Votre Mot De Passe: `.yellow)
-
 
 console.clear()
 
@@ -16,9 +13,34 @@ let req = JSON.parse(request("POST", 'https://discord.com/api/v8/auth/login', {
         "password": password
     }
 }).body)
+if (req.code === 50035) {
 
+    console.clear();
+    console.log("Email Ou Mot De Passe Invalide".red)
+
+} else {
+    if (req.captcha_key) {
+
+        console.clear()
+        console.log("Impossible De Vous Donnez Votre Token".red)
+
+    } else {
+
+        if (req.ticket) {
+            console.clear()
+            deuxfa0()
+
+        } else {
+
+            console.clear()
+            console.log(`Voici ton token: ${req.token}`.green)
+
+        }
+    }
+}
 function deuxfa0() {
-    var deuxfa = question.question((`Veuillez Entrer Le Code De Doubles Vérifications: `.cyan))
+
+    const deuxfa = question.question(`Veuillez Entrer Le Code De Doubles Vérifications: `.cyan)
 
     let res = JSON.parse(request("POST", 'https://discord.com/api/v8/auth/mfa/totp', {
         json: {
@@ -26,33 +48,14 @@ function deuxfa0() {
             "ticket": req.ticket
         }
     }).body)
+
     if (res.message) {
         console.clear()
-
-        console.log(`Le Code A2F est Invalide`.red);
+        console.log(`Le Code A2F est Invalide`.red)
         deuxfa0()
 
     } else {
-
         console.clear();
-
         console.log(`Voici ton token: ${res.token}`.green)
     }
-}
-
-if (req.code === 50035) {
-    console.clear();
-    console.log("Email Ou Mot De Passe Invalide".red)
-    
-} else if (req.captcha_key) {
-    console.clear();
-    console.log(("Impossible De Vous Donnez Votre Token".red));
-    
-} else if (req.ticket) {
-    console.clear()
-    deuxfa0()
-
-} else {
-    console.clear()
-    console.log((`Voici ton token: ${req.token}`.green))
 }
